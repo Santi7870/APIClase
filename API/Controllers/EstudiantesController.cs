1f83510be695b67ab00fcb5a25a47b57a518114b
santi7870 <santiago.panchi@udla.edu.ec>
2024-10-25 08:58:05 -0500

    commit2

--- API/Controllers/EstudiantesController.cs
+++ API/Controllers/EstudiantesController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Models;
 using API.Respository;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -16,5 +17,41 @@ namespace API.Controllers
             var listado = repo.DevuelveListdoEstudiantes();
             return listado;
             }
+        [HttpGet]
+        [Route("InfromacionEudiante/{id}")]
+        public Action DevuelveInformacionEstudiante(int id)
+        {
+            try
+            {
+                EstudiantesRespository repo = new EstudiantesRespository();
+                var listado = repo.DevuelveListdoEstudiantes();
+                    if (listado.Any(x => x.Id == id))
+                    {
+                    var estudiante = listado.Find(x => x.Id == id);
+                    return Ok(estudiante);
+
+                    }
+                    else
+                    {
+                    return NotFound();
+                    }
+    
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+
+            }
+        }
+        [HttpPost]
+        public ActionResult GuardaEstudiante([FromBody]Estudiante estudiante)
+        {
+            return Ok();
+
+        }
+
+
+
+        }
     }
-}
+    
